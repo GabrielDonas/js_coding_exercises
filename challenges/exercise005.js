fc146c62d66ee792f89b967097b179c5b07fa65d
Gabriel Donas <osradecky@gmail.com>
2021-04-07 22:51:14 +0100

    Implemented exercise005 findNeedle

--- challenges/exercise005.js
+++ challenges/exercise005.js
@@ -17,28 +17,61 @@ const count1sand0s = (str) => {
 
 const reverseNumber = (n) => {
   if (n === undefined) throw new Error("n is required");
-  //CHECK
+  let nString = n.toString();
+  let nArrayReversed = nString.split("").reverse();
+  let nStringReversed = nArrayReversed.join("");
+  return parseFloat(nStringReversed);
 };
 
 const sumArrays = (arrs) => {
   if (arrs === undefined) throw new Error("arrs is required");
-  // Your code here!
+  let arrSum = 0;
+  arrs.forEach((arr) => (arrSum += arr.reduce((a, b) => a + b, 0)));
+  return arrSum;
 };
 
 const arrShift = (arr) => {
   if (arr === undefined) throw new Error("arr is required");
-  // Your code here!
+  if (arr.length >= 2) {
+    [arr[0], arr[arr.length - 1]] = [arr[arr.length - 1], arr[0]];
+  }
+  return arr;
 };
 
 const findNeedle = (haystack, searchTerm) => {
   if (haystack === undefined) throw new Error("haystack is required");
   if (searchTerm === undefined) throw new Error("searchTerm is required");
-  // Your code here!
+  let arr = Object.values(haystack);
+  let str = arr.join().toLowerCase();
+  return str.indexOf(searchTerm.toLowerCase()) == -1 ? false : true;
 };
 
 const getWordFrequencies = (str) => {
   if (str === undefined) throw new Error("str is required");
-  // Your code here!
+  let arr = str.split(" ");
+  let newArr = [];
+  //clean words and insert them into the new array as key-value pairs
+  for (let i = 0; i < arr.length; i++) {
+    let word = arr[i].toLowerCase().replace(/[^a-zA-Z0-9]/g, "");
+    newArr.push([word, 1]);
+  }
+
+  //turn the key-value pairs array into an Object
+  let entries = new Map(newArr);
+  let wordsObj = Object.fromEntries(entries);
+
+  //console.log(wordsObjKeys);
+  let wordsObjKeys = Object.keys(wordsObj);
+  for (let j = 0; j < wordsObjKeys.length; j++) {
+    for (let x = 0; x < newArr.length; x++) {
+      if (wordsObjKeys[j] === newArr[x][0]) {
+        //console.log(wordsObj[0]);
+        //wordsObj[j]++;
+      }
+    }
+  }
+  console.log(wordsObj[0]);
+  return wordsObj;
 };
 
 module.exports = {
